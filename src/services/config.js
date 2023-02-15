4e5ea8d4331aa6ec698217a3a224d375c6305a2a
DaoQuangThiet <67154017+DaoQuangThiet@users.noreply.github.com>
2023-02-15 14:42:12 +0700

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,3 @@
-const medusaUrl = __MEDUSA_BACKEND_URL__ || "https://medusaserver-production.up.railway.app/"
+const medusaUrl = __MEDUSA_BACKEND_URL__ || "https://medusaserver-production.up.railway.app"
 
 export { medusaUrl }
